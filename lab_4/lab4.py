834e93712e242ccc4572caf85b01034624058582
Lorenzo Scarrone <s265467@studenti.polito.it>
2019-11-25 16:48:30 +0100

    Laboratorio 4

--- lab_4/lab4.py
+++ lab_4/lab4.py
@@ -12,3 +12,19 @@ si = data["SI"][0]
 #creating spectral information
 obj = create_input_spectral_information(si["f_min"],si["f_max"],si["roll_off"],si["baud_rate"],10**((si["power_dbm"]-30)/10),si["spacing"])
 
+#replacing value as requested
+obj1 = obj._replace(carriers=tuple(c._replace(power = c.power._replace(signal= c.power.signal + 10**0.3, nli = 5*10**(-8), ase= 10**(-7)))
+                              for c in obj.carriers))
+
+#dati plot
+p_,f_ = [],[]
+ase_,nli_ = [],[]
+
+#making list to plot
+for ogg in obj1[1]:
+	f_.append(ogg.frequency)
+	p_.append(ogg.power.signal)
+	ase_.append(ogg.power.ase)
+	nli_.append(ogg.power.nli)
+
+
