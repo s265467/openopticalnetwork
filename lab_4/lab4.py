fe0304ff60ccf811321a884ba19f6167a7a4d824
Lorenzo Scarrone <s265467@studenti.polito.it>
2019-11-25 15:44:39 +0100

    Laboratorio 4

--- lab_4/lab4.py
+++ lab_4/lab4.py
@@ -3,19 +3,12 @@ import json
 import matplotlib.pyplot as plt
 from math import *
 
-with open("spectral_info.json","r") as read_file:
+#reading json file
+with open("dati_esercizio.json","r") as read_file:
 	data = json.load(read_file)
-n=[]
-for el in data["spectral_info"][0]:
-	n.append( float(data["spectral_info"][0][el]) )
 
-test = create_input_spectral_information(n[0],n[1],n[2],n[3],n[4],n[5])
+#obtaining data to compute spectral information
+si = data["SI"][0]
+#creating spectral information
+obj = create_input_spectral_information(si["f_min"],si["f_max"],si["roll_off"],si["baud_rate"],10**((si["power_dbm"]-30)/10),si["spacing"])
 
-x_,y_ = [],[]
-for el in test[1]:
-	x_.append(el[1]/(10**12))
-	y_.append(10*log10(el[4][0])+30)
-plt.ylabel("[dBm] (power of signals)")
-plt.xlabel("[THz] channels frequency")
-plt.plot(x_,y_,'o')
-plt.show()
